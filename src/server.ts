00fbd189079e37fdb65d55764aee9fe618c0707e
MAHFUZ ISLAM <77238922+Mahfuzislam1695@users.noreply.github.com>
2023-08-27 22:58:58 +0600

    database connection

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function database() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log(`Databse connected successfully`);
+
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(`Failed to connect to Mongo`);
+  }
+}
+
+database();
