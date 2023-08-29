614cfc730f13e7aa6a8193159fc23aefdc3cc40a
MAHFUZ ISLAM <77238922+Mahfuzislam1695@users.noreply.github.com>
2023-08-29 19:19:38 +0600

    user basic

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import { UserRoutes } from './app/modules/user/user.route';
 
 const app: Application = express();
 
@@ -6,6 +7,10 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// * Application routes
+
+app.use('/api/v1/users/', UserRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
